c63f1f1f6430abed177664e2849181565a2420d2
Carlos Cosming <carlos@kingly.cl>
2024-08-16 20:57:14 -0400

    ✅ test(aggregates): add user event code for aggregate root

--- packages/aggregates/src/tests/common/user-event.ts
+++ packages/aggregates/src/tests/common/user-event.ts
@@ -2,3 +2,5 @@ import { Event } from '../../lib';
 import { type UserProps } from './user-entity';
 
 export class UserEvent extends Event<UserProps, 'USER_CREATED_EVENT' | 'USER_DELETED_EVENT'> {}
+
+export type UserEventCode = UserEvent['code'];
